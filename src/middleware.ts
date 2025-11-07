74baa3814336a69adb5476675bab4aa32c2f3fad
Narendra Mali <narendrakmali@gmail.com>
2025-11-07 19:10:21 +0000

    Why My Webapp not functioning correctly

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,4 @@
+
 import createMiddleware from 'next-intl/middleware';
  
 export default createMiddleware({
@@ -10,16 +11,5 @@ export default createMiddleware({
  
 export const config = {
   // Match only internationalized pathnames
-  matcher: [
-    // Enable a redirect to a matching locale at the root
-    '/',
-
-    // Set a cookie to remember the previous locale for
-    // all requests that have a locale prefix
-    '/(hi|mr|en)/:path*',
-
-    // Enable redirects that add a locale prefix
-    // (e.g. `/pathnames` -> `/en/pathnames`)
-    '/((?!_next|_vercel|.*\\..*).*)'
-  ]
+  matcher: ['/', '/(hi|mr|en)/:path*']
 };
