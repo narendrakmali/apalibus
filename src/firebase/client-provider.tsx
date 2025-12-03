37c63cf99ca8036a915d0c9c8ab958cf9ffb7a4e
Narendra Mali <narendrakmali@gmail.com>
2025-12-03 18:58:21 +0000

    Try fixing this error: `Runtime Error: Error: Cannot read properties of

--- src/firebase/client-provider.tsx
+++ src/firebase/client-provider.tsx
@@ -1,13 +1,12 @@
 
 'use client';
 
-import * as React from 'react';
-import { ReactNode } from 'react';
+import { ReactNode, useMemo } from 'react';
 import { getSdks, initializeFirebase } from '.';
 import { FirebaseProvider } from './provider';
 
 export function FirebaseClientProvider({ children }: { children: ReactNode }) {
-  const firebaseContext = React.useMemo(() => {
+  const firebaseContext = useMemo(() => {
     const { firebaseApp, ...sdks } = initializeFirebase();
     return { ...getSdks(firebaseApp), firebaseApp };
   }, []);
