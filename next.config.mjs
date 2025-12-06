75e67e50c3b5184655e6a8dc27add73113e21573
Narendra Mali <narendrakmali@gmail.com>
2025-12-06 13:28:02 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
-import type {NextConfig} from 'next';
- 
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
@@ -23,7 +22,7 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'https',
+        protocol: 'https,',
         hostname: 'picsum.photos',
         port: '',
         pathname: '/**',
