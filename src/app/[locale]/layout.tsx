c088e6fda7d02c6a6247b37a9ec24aed9b150434
Narendra Mali <narendrakmali@gmail.com>
2025-12-11 20:36:05 +0000

    This error happens because a component (likely your **Header**) is tryin

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,9 +1,9 @@
-
 import { notFound } from 'next/navigation';
-import {NextIntlClientProvider, useMessages} from 'next-intl';
+import { NextIntlClientProvider } from 'next-intl';
+import { getMessages } from 'next-intl/server';
+import { FirebaseClientProvider } from '@/firebase';
 import Header from "@/components/header";
 import Footer from "@/components/footer";
-import { getMessages } from 'next-intl/server';
 
 const locales = ['en', 'hi', 'mr'];
 
@@ -18,19 +18,19 @@ export default async function LocaleLayout({
     notFound();
   }
 
-  // Providing all messages to the client
-  // side is the easiest way to get started
   const messages = await getMessages();
 
   return (
     <html lang={locale}>
       <body>
         <NextIntlClientProvider locale={locale} messages={messages}>
+          <FirebaseClientProvider>
             <Header />
             <main className="flex-grow bg-background">
               {children}
             </main>
             <Footer />
+          </FirebaseClientProvider>
         </NextIntlClientProvider>
       </body>
     </html>
